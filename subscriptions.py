65b0b261fa891bc71b25e458018aecd45a4b0a27
KarthikVajja <karthik31032000@gmail.com>
2022-03-07 10:45:36 +0530

    added comments to subs

--- subscriptions.py
+++ subscriptions.py
@@ -8,21 +8,24 @@ driver = uc.Chrome(executable_path='<path to your driver>')
 
 #Login
 try:
+    #Open youtube login page
     driver.get(r'https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den-GB%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=en-GB&ec=65620&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
     driver.implicitly_wait(15)
-  
+    
+    #Enter email
     loginBox = driver.find_element_by_xpath('//*[@id ="identifierId"]')
     print('Enter email:', end=' ')
     email = input()
-  
+    
     nextButton = driver.find_elements_by_xpath('//*[@id ="identifierNext"]')
     nextButton[0].click()
   
+    #Enter password
     passWordBox = driver.find_element_by_xpath(
         '//*[@id ="password"]/div[1]/div / div[1]/input')
     print('Enter password:', end=' ')
     password = input()
-  
+    
     nextButton = driver.find_elements_by_xpath('//*[@id ="passwordNext"]')
     nextButton[0].click()
   
@@ -31,10 +34,12 @@ try:
 except:
     print('Login Failed')
 
+#Import subscriptions.csv file
 df = pandas.read_csv('<path to your csv file>')
 subs = np.array(df['Channel URL'])
-print(subs)
+print(subs)     #All channels the user is subscribed to
 
+#Open all channels and subscribe
 for i in subs:
     driver.get(i)
     sub_button = driver.find_element_by_xpath('//*[@id="subscribe-button"]')
